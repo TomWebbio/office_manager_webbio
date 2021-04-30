a54b27c8beda6e90ecd28a0c3c8cfee6bde2a31c
Tom Klein <tom@webbio.nl>
2021-04-30 10:09:09 +0200

    feat: added next

---
+++ apps/office-manager-client/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'office-manager-client',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/office-manager-client',
+};
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  projects: ['<rootDir>/apps/office-manager-server'],
+  projects: [
+    '<rootDir>/apps/office-manager-server',
+    '<rootDir>/apps/office-manager-client',
+  ],
 };
