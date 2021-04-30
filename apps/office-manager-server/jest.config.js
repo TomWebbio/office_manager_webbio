55860f24d4bc5fc247b3a8bf417e7bea457a1c47
Tom Klein <tom@webbio.nl>
2021-04-30 09:18:43 +0200

    initial commit

---
+++ apps/office-manager-server/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'office-manager-server',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/office-manager-server',
+};
